b8bbf846c456c47368910b83d825f5d8ae283018
AlucardSanin <alucardsanin@gmail.com>
2022-08-01 18:52:43 -0400

    Books Functions

--- js/app.js
+++ js/app.js
@@ -1,17 +1,87 @@
 
-let books=["hello", "test"];
-let list = document.querySelector('.awesomeList')
+//Model Section
+let books;
 
+//Retrieve localStorage
+const savedBooks = JSON.parse(localStorage.getItem ('books'));
 
-books.forEach(function(addToLib) {
-  let element = document.createElement('div')
-  element.innerText = addToLib;
-  document.body.appendChild(element);
-});
+if (Array.isArray(savedBooks)){
+books = savedBooks;
+}else{
+  books  = [{
+    title:'Das' , 
+    author:'Doy',
+    id: '1'
+  }];
+}
 
+render();
+
+let list = document.querySelector('.awesomeList');
+
+    //Save a Book
+function createBooks(title, author) {
+  const id = '' + new Date().getTime();
+
+  books.push({
+    title: title,
+    author: author,
+    id: id
+  });
+
+  saveBooks();
+}
+
+     //Deletes a Book
+function removeBooks(idToDelete){
+  books = books.filter(function (books){
+    if (books.id !== idToDelete){
+      return true;
+    }else{
+      return false;
+    }
+  });
+
+  saveBooks();
+}
+
+function saveBooks(){
+  localStorage.setItem('books', JSON.stringify(books));
+}
+
+//Controller
 function addBook () {
   let titletextbox =document.getElementById('title');
   let authortextbox = document.getElementById('author');
-  let  title = titletextbox.value;
-  console.log(books);
+  let title = titletextbox.value;
+  let author = authortextbox.value;  
+
+  createBooks(title, author);
+  render();
+}
+
+function deleteBook (event){
+  const deleteButton = event.target;
+  const idToDelete = deleteButton.id;  
+  removeBooks(idToDelete);
+  render();
+}
+
+//View
+function render(){
+  document.querySelector('.awesomeList').innerHTML = '';
+  books.forEach(function(books) {    
+    let List = document.querySelector('.awesomeList');
+    let element = document.createElement('div');
+    element.innerText = books.title +  '\n'  + books.author + '\n';
+
+    //Remove Button
+    const deleteButton = document.createElement('button');
+    deleteButton.innerText =  'Remove'; 
+    deleteButton.onclick = deleteBook;    
+    deleteButton.id = books.id;
+    
+    List.appendChild(element);
+    element.appendChild(deleteButton)
+  });
 }
