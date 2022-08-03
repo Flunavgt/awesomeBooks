860faff6ba766c3daf448d60fdd5e11afc03fdc6
Federico <fluna1973@yahoo.com>
2022-08-03 12:19:16 -0600

    correct indent

--- js/logic.js
+++ js/logic.js
@@ -6,31 +6,31 @@ myBookshelf.LoadBooksFromLocal();
 
 const render = () => {
   document.querySelector('.awesomeList').innerHTML = '';
-  if(myBookshelf.Books){
-  myBookshelf.Books.forEach((books) => {
-    const List = document.querySelector('.awesomeList');
-    const element = document.createElement('div');
-    element.classList.add('eachBook');
-    element.innerText = `${books.title} by ${books.author}`;
-    List.appendChild(element);
+  if (myBookshelf.Books) {
+    myBookshelf.Books.forEach((books) => {
+      const List = document.querySelector('.awesomeList');
+      const element = document.createElement('div');
+      element.classList.add('eachBook');
+      element.innerText = `${books.title} by ${books.author}`;
+      List.appendChild(element);
 
-    function deleteBook(event) {
-      const deleteButton = event.target;
-      const idToDelete = deleteButton.id;
-      myBookshelf.DeleteBook(idToDelete);
-      myBookshelf.SaveBookshelfLocal();
-      render();
-    }
+      function deleteBook(event) {
+        const deleteButton = event.target;
+        const idToDelete = deleteButton.id;
+        myBookshelf.DeleteBook(idToDelete);
+        myBookshelf.SaveBookshelfLocal();
+        render();
+      }
 
-    // Remove Button
-    const deleteButton = document.createElement('button');
-    deleteButton.innerText = 'Remove';
-    deleteButton.classList.add('delButton');
-    deleteButton.onclick = deleteBook;
-    deleteButton.id = books.id;
-    element.appendChild(deleteButton);
-  });
-}
+      // Remove Button
+      const deleteButton = document.createElement('button');
+      deleteButton.innerText = 'Remove';
+      deleteButton.classList.add('delButton');
+      deleteButton.onclick = deleteBook;
+      deleteButton.id = books.id;
+      element.appendChild(deleteButton);
+    });
+  }
 };
 
 // Controller
